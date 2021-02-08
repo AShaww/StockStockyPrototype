0ec668972d329090fe236c190477e16e258cb5f1
Amir Shaw <AmirShaw@hotmail.co.uk>
2021-02-08 17:34:51 +0000

    App Broke,
    Remade MainActivity

--- app/src/main/java/com/example/stockstockyprototype/DataBaseHelper.java
+++ app/src/main/java/com/example/stockstockyprototype/DataBaseHelper.java
@@ -9,15 +9,22 @@ import androidx.annotation.Nullable;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
-        public static final WINE_TABLE ="WINE_TABLE";
-
-
         public static final String ITEM_NAME = "";
 
 
         public static final String WINE_STOCK_TAKE = "WINE_STOCK_TAKE";
         public static final String ITEM_CODE = "ITEM_CODE";
         public static final String WEEK_N_73_PAR = "WEEK_N73_PAR";
+        public static final String STOREROOM_FRIDGE = "STOREROOM_FRIDGE";
+        public static final String LOWER_GROUND_RACK = "LOWER_GROUND_RACK";
+        public static final String GROUND_FLOOR_FRIDGE = "GROUND_FLOOR_FRIDGE";
+        public static final String GROUND_FLOOR_BA_INC_RETAILER = "GROUND_FLOOR_BA_INC_RETAILER";
+        public static final String FIRST_FLOOR_GLASS_RACK = "FIRST_FLOOR_GLASS_RACK";
+        public static final String FIRST_FLOOR_STAINLESS_SHELF = "FIRST_FLOOR_STAINLESS_SHELF";
+        public static final String FIRST_FLOOR_BAR_SPEED_RAIL = "FIRST_FLOOR_BAR_SPEED_RAIL";
+        public static final String TOTAL_IN_STOCK = "TOTAL_IN_STOCK";
+        public static final String VARIANCE = "VARIANCE";
+        public static final String ACTIVE_WINE = "ACTIVE_WINE";
 
         public DataBaseHelper(@Nullable Context context) {
                 super(context, "redWine.db", null, 1);
@@ -26,7 +33,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-                String createTableStatement = "CREATE TABLE " + WINE_STOCK_TAKE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + ITEM_CODE + ", " + WEEK_N_73_PAR + " TEXT, STOREROOM_FRIDGE,LOWER_GROUND_RACK, GROUND_FLOOR_FRIDGE, GROUND_FLOOR_BA_INC_RETAILER, FIRST_FLOOR_GLASS_RACK,FIRST_FLOOR_STAINLESS_SHELF,FIRST_FLOOR_BAR_SPEED_RAIL, TOTAL_IN_STOCK, VARIENCE, ACTIVE_WINE)";
+                String createTableStatement = "CREATE TABLE " + WINE_STOCK_TAKE + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + ITEM_CODE + ", " + WEEK_N_73_PAR + " , " + STOREROOM_FRIDGE + "," + LOWER_GROUND_RACK + ", " + GROUND_FLOOR_FRIDGE + ", " + GROUND_FLOOR_BA_INC_RETAILER + ", " + FIRST_FLOOR_GLASS_RACK + "," + FIRST_FLOOR_STAINLESS_SHELF + "," + FIRST_FLOOR_BAR_SPEED_RAIL + ", " + TOTAL_IN_STOCK + ", " + VARIANCE + ", " + ACTIVE_WINE + ")";
                 db.execSQL(createTableStatement);
         }
 
